50113e52d09ddc96c1d52bbcc948030496aa0517
Mehmet Yılmaz <35243461+ylmazmehmet60@users.noreply.github.com>
2024-01-01 01:38:18 +0300

    index.js is added.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const InstagramLogin = require('./InstagramLogin');
 const WebProfileInfo = require('./Services/WebProfileInfo');
 const UserFeed = require('./Services/UserFeed');
+const FriendShip =  require('./Services/FriendShip') ;
 
 module.exports = {
   InstagramLogin,
   WebProfileInfo,
-  UserFeed
+  UserFeed,
+  FriendShip
 };
